d000c78a96e4f5fa4e730e9b50c485e34fe68460
Truls Matias Gederø Torgersen <trulsmatias@gmail.com>
2023-05-14 10:59:07 +0200

    text selection splash screen. iOS generated code

--- lib/splash/about.dart
+++ lib/splash/about.dart
@@ -8,37 +8,39 @@ class About extends StatelessWidget {
     return Container(
       color: Theme.of(context).primaryColor, //Colors.green,
       width: MediaQuery.of(context).size.width,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(leftPadding, 20, 8, 0),
-            child: Text(
-              "Your digital minigolf scorecard.\n",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
+      child: SelectionArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(leftPadding, 20, 8, 0),
+              child: Text(
+                "Your digital minigolf scorecard.\n",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(leftPadding, 0, 25, 20),
-            child: Text(
-              "Use the app instead of a piece of paper. Then share the game with the other players!\n\n"
-              "- The score is calculated instantly while you play (optional)\n"
-              "- Customize holes' names, add an extra hole or delete it to fit your course\n"
-              "- The games are saved to look back on later\n"
-              "- Share the game with others\n"
-              "- Statistics! Who has the most hole in ones? Or who is the most consistent?",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                height: 1.5,
+            Padding(
+              padding: EdgeInsets.fromLTRB(leftPadding, 0, 25, 20),
+              child: Text(
+                "Use the app instead of a piece of paper. Then share the game with the other players!\n\n"
+                "- The score is calculated instantly while you play (optional)\n"
+                "- Customize holes' names, add an extra hole or delete it to fit your course\n"
+                "- The games are saved to look back on later\n"
+                "- Share the game with others\n"
+                "- Statistics! Who has the most hole in ones? Or who is the most consistent?",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  height: 1.5,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
